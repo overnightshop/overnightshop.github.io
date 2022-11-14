40b4656fc8831646ba99b9c3e675761091604ac8
overnightshop <116987476+overnightshop@users.noreply.github.com>
2022-11-13 21:09:08 -0700

    PHP main

--- main.php
+++ main.php
@@ -3,12 +3,22 @@
   <head>
     <meta charset="UTF-8"/>
     <title>Overnight Shop</title>
-    <% # Code By: Maysun Campos %>
     <link rel="stylesheet" href="style.css" type="text/css"/>
   </head>
   <body dir="ltr">
     <main>
       <h1>Overnight Shop</h1>
+      <?php 
+      $log = false;
+      
+      if ($log == true) {
+        echo "Welcome Back";
+      } else if ($log == false) {
+        echo "Browsing As Guest";
+      } else {
+        // do nothing
+      }
+      ?>
     </main>
   </body>
 </html>
